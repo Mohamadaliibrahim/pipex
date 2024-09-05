2bbf2a88205989242de4c8041affbe68d16b7800
Mohammad Ali Ibrahim <mohamibr@c1r6s2.42beirut.com>
2024-09-05 15:11:54 +0300

    final upload

---
+++ pipex/utils2.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohamibr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 15:03:10 by mohamibr          #+#    #+#             */
+/*   Updated: 2024/09/05 15:03:11 by mohamibr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+char	*find_place_of_path(char **av, char **env)
+{
+	char	*path_without_split;
+	char	*str;
+	char	**path;
+	char	*place;
+
+	if (access(av[2], F_OK) == 0)
+		return (ft_strdup(av[2]));
+	find_the_files(av);
+	str = find_path(env);
+	if (!str)
+	{
+		ft_printf("Error: PATH not found\n");
+		exit(1);
+	}
+	path_without_split = ft_strdup(&str[5]);
+	free(str);
+	path = ft_split(path_without_split, ':');
+	free(path_without_split);
+	place = after_split(path, av[2]);
+	lets_free(path);
+	return (place);
+}
+
+char	*find_place_of_path2(char **av, char **env)
+{
+	char	*path_without_split;
+	char	*str;
+	char	**path;
+	char	*place;
+
+	if (access(av[3], F_OK) == 0)
+		return (ft_strdup(av[3]));
+	find_the_files(av);
+	str = find_path(env);
+	if (!str)
+	{
+		ft_printf("Error: PATH not found\n");
+		exit(1);
+	}
+	path_without_split = ft_strdup(&str[5]);
+	free(str);
+	path = ft_split(path_without_split, ':');
+	free(path_without_split);
+	place = after_split(path, av[3]);
+	lets_free(path);
+	return (place);
+}
+
+void	parse_cmd1(t_list *list, char **env, char **av)
+{
+	char	*place;
+	char	**cmd;
+
+	place = find_place_of_path(av, env);
+	cmd = ft_split(list->cmd1, ' ');
+	if (!cmd)
+	{
+		ft_printf("Error\n");
+		exit(1);
+	}
+	free(list);
+	if (execve(place, cmd, env) == -1)
+	{
+		lets_free(cmd);
+		p_error_list(NULL);
+	}
+	lets_free(cmd);
+}
+
+void	parse_cmd2(t_list *list, char **env, char **av)
+{
+	char	*place;
+	char	**cmd;
+
+	place = find_place_of_path2(av, env);
+	cmd = ft_split(list->cmd2, ' ');
+	if (!cmd)
+	{
+		free(place);
+		ft_printf("Error\n");
+		exit(1);
+	}
+	free(list);
+	if (execve(place, cmd, env) == -1)
+	{
+		free(place);
+		ft_printf("Error\n");
+		exit(1);
+	}
+}
+
+void	lets_fork1(char **env, int pipe_list[2], t_list *list, char **av)
+{
+	int	fd;
+
+	fd = open(list->infile, O_RDONLY);
+	if (fd == -1)
+		p_error_list(list);
+	if (dup2(fd, STDIN_FILENO) == -1)
+		p_error_list(list);
+	if (dup2(pipe_list[1], STDOUT_FILENO) == -1)
+		p_error_list(list);
+	close(pipe_list[0]);
+	close(pipe_list[1]);
+	close(fd);
+	parse_cmd1(list, env, av);
+}
---
+++ pipex/utils3.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils3.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mohamibr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 15:03:17 by mohamibr          #+#    #+#             */
+/*   Updated: 2024/09/05 15:03:19 by mohamibr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	lets_fork2(char **env, int pipe_list[2], t_list *list, char **av)
+{
+	int	fd;
+
+	fd = open(list->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+		p_error_list(list);
+	if (dup2(pipe_list[0], STDIN_FILENO) == -1)
+		p_error_list(list);
+	if (dup2(fd, STDOUT_FILENO) == -1)
+		p_error_list(list);
+	close(pipe_list[0]);
+	close(pipe_list[1]);
+	close(fd);
+	parse_cmd2(list, env, av);
+}
+
+void	find_the_files(char **av)
+{
+	t_list	*list;
+
+	list = init_list_av(av);
+	if (!list->infile || list->infile[0] == '\0')
+		p_error_list(list);
+	if (!list->outfile || list->outfile[0] == '\0')
+		p_error_list(list);
+	free(list);
+}
